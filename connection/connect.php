1809a4bac9c669fc81c9325ebe8117ed21ae793a
tanshaojun12 <51236955+tanshaojun12@users.noreply.github.com>
2024-01-02 21:56:59 +0800

    update database name

--- connection/connect.php
+++ connection/connect.php
@@ -4,7 +4,7 @@
 $servername = "localhost"; //server
 $username = "root"; //username
 $password = ""; //password
-$dbname = "onlinefoodphp";  //database
+$dbname = "richiamocoffee";  //database
 
 // Create connection
 $db = mysqli_connect($servername, $username, $password, $dbname); // connecting 
